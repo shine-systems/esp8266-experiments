c2300e8c50f5f75f1a7475cdb40579d12a475994
Sebastien Ringrose <seb@codeworks.me>
2023-06-29 16:07:38 +0100

    feat: using lambdas for cleaner Sensor and Controller definitions

--- envi_soil/main.ino
+++ envi_soil/main.ino
@@ -1,61 +1,66 @@
 #include <vector>
-#include <Adafruit_AHTX0.h>
 #include <Shinode.h>
+#include <Adafruit_AHTX0.h>
 
-Adafruit_AHTX0 aht;
-Adafruit_Sensor *aht_humidity, *aht_temp;
 int SOIL_MOISTURE = A0;
-float soil_moisture_value = 0.0;
-
-void moistureSensorSetup() {
-  pinMode(SOIL_MOISTURE, OUTPUT);
-}
-
-String moistureSensorSense() {
-  float value = analogRead(SOIL_MOISTURE);
-  int max = 700;
-  int min = 200;
-  float calibrated = ((value - min) / (max - min)) * 100;
-  return String(calibrated);
-}
-
 Sensor moisture_sensor = {
   String("soil_moisture"),
   String("%"),
-  moistureSensorSetup,
-  moistureSensorSense
+  [=]() { pinMode(SOIL_MOISTURE, OUTPUT); },
+  [=]() {
+    float value = analogRead(SOIL_MOISTURE);
+    int max = 700;
+    int min = 200;
+    float calibrated = ((value - min) / (max - min)) * 100;
+    return String(calibrated);
+  }
 };
 
-void climateSensorSetup() {
-  if (!aht.begin()) {
-    Serial.println("Failed to find AHT10/AHT20 chip");
-    while (1) {
-      delay(10);
+Adafruit_AHTX0 aht;
+Adafruit_Sensor *aht_temp;
+Sensor temperature_sensor = {
+  String("temperature"),
+  String("degrees C"),
+  [&]() { 
+    if (!aht.begin()) {
+      Serial.println("Failed to find AHT10/AHT20 chip");
+      while (1) {
+        delay(10);
+      }
     }
+
+    aht_temp = aht.getTemperatureSensor();
+    aht_temp->printSensorDetails();
+  },
+  [&]() {
+    sensors_event_t temp;
+    aht_temp = aht.getTemperatureSensor();
+    aht_temp->getEvent(&temp);
+    return String(temp.temperature);
   }
+};
 
-  Serial.println("AHT10/AHT20 Found!");
-  aht_temp = aht.getTemperatureSensor();
-  aht_temp->printSensorDetails();
+Adafruit_Sensor *aht_humidity;
+Sensor humidity_sensor = {
+  String("humidity"),
+  String("%H"),
+  [=]() { 
+    if (!aht.begin()) {
+      Serial.println("Failed to find AHT10/AHT20 chip");
+      while (1) {
+        delay(10);
+      }
+    }
 
-  aht_humidity = aht.getHumiditySensor();
-  aht_humidity->printSensorDetails();
-}
-
-String climateSensorSense() {
-  sensors_event_t humidity;
-  sensors_event_t temp;
-  aht_humidity->getEvent(&humidity);
-  aht_temp->getEvent(&temp);
-
-  return String("Humidity: " + String(humidity.relative_humidity) + "% rH" + " Temperature: " + String(temp.temperature) + " degrees C");
-}
-
-Sensor climate_sensor {
-  String("climate"),
-  String("humidity and temperature"),
-  climateSensorSetup,
-  climateSensorSense
+    aht_humidity = aht.getHumiditySensor();
+    aht_humidity->printSensorDetails();
+  },
+  [=]() {
+    sensors_event_t humidity;
+    aht_humidity = aht.getHumiditySensor();
+    aht_humidity->getEvent(&humidity);
+    return String(humidity.relative_humidity);
+  }
 };
 
 Shinode device(
@@ -65,7 +70,7 @@ Shinode device(
   APPSK,
   host,
   rootCACert,
-  std::vector<Sensor>{ moisture_sensor, climate_sensor },
+  std::vector<Sensor>{ moisture_sensor, temperature_sensor, humidity_sensor },
   std::vector<Controller>{}
 );
 
